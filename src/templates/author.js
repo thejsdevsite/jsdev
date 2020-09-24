38a51b09152605d80c66d180c41e2b07a0164bcc
Justin Mitchell <justin.mitchell@auspost.com.au>
2020-09-24 15:43:07 +1000

    Adding mobile optimized presentation for tag and author details and some SEO fixes

--- src/templates/author.js
+++ src/templates/author.js
@@ -3,14 +3,22 @@ import { graphql } from "gatsby"
 import LayoutAuthor from "../components/layoutAuthor"
 import PostRollByAuthorId from "../components/postRollByAuthorId"
 import SEO from "../components/seo"
+import useGetAuthorDetailsStatic from "../hooks/static/getAuthorDetailsStatic"
+import RenderSidebarAuthorDetails from "../components/sidebar/renderSidebarAuthorDetails"
 
 const AuthorTemplate = ({ pageContext, data, location }) => {
   const { author } = pageContext;
   const siteTitle = data.site.siteMetadata?.title || `Title`
+  const authorDetails = useGetAuthorDetailsStatic(author);
 
   return (
     <LayoutAuthor location={location} title={siteTitle} authorId={author}>
-      <SEO title="All Posts by author" />
+      <SEO title={`${authorDetails.first().name} - All Posts by author`} />
+
+      <div className="hide show-tablet jsd-header-widget">
+        <RenderSidebarAuthorDetails author={authorDetails.first()} />
+      </div>
+
       <header className="mb-2">
         <h2 className="fs-m fw-heavy m-0 ff-monospace">Posts</h2>
       </header>
